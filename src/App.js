a35f1ce10e64c7135da722928e2c318d0c1b0218
Dylan Frederick <93879414+dylanLf47@users.noreply.github.com>
2024-02-23 14:13:14 -0800

    Added log in screen and updated get to only get PlayerCharacters with matching userId.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,33 @@ import './App.css';
 
 let i = 0;
 
+function LogIn({theLogIn, logInClick}) {
+    const [userID, setUserID] = useState('')
+    const [userIdHover, setUserIDHover] = useState(false)
+    const [invalidInput, setInvalidInput] = useState(true)
+    const update = () => {
+        logInClick(theLogIn, userID)
+    }
+    useEffect(() => {
+        if (!/^[\w ,.'-]{0,20}$/.test(userID)) {
+            setInvalidInput(true)
+        }
+        else setInvalidInput(false)
+    },[userID]);
+    return (
+        <div>
+        <h1 style={{margin: 0}}>DLF's DPR Calculator</h1>
+        <div className='logInBox'>
+            <div><b>Enter Your User ID</b></div>
+            <div><input type='text' placeholder="User ID" className={!/^[\w ,.'-]{0,20}$/.test(userID) ? "errorBorder":""} value={userID} onChange={(e) => {setUserID(e.target.value)}} maxLength={20} onMouseEnter={() => setUserIDHover(true)} onMouseLeave={() => setUserIDHover(false)}></input></div>
+            {userIdHover && <span className='inputPromptLogIn'>User ID must include alphabetical/numerical characters and/or symbols including ,.'-</span>}
+            <span>&nbsp;</span>
+            <button onClick={() => update()} disabled={invalidInput}>Log In</button>
+        </div>
+        </div>
+    )
+}
+
 function Dice({update, remove, gwfIsChecked, eaIsChecked, saIsChecked}) {
     const [diceAmount, setDiceAmount] = useState('')
     const [diceNum, setDiceNum] = useState('4')
@@ -95,7 +122,7 @@ function Dice({update, remove, gwfIsChecked, eaIsChecked, saIsChecked}) {
       );
 }
 
-function Calc() {
+function Calc(userId) {
     const [name,setName] = useState('')
     const [description,setDescription] = useState('')
 
@@ -384,7 +411,7 @@ function Calc() {
 
     const addCharacter = (e) => {
         e.preventDefault()
-        const player_character = {name, description, crit_range: critRange, attack_mod: attackMod, num_of_attacks: numOfAttacks, has_adv: hasAdv, standard_die_list: standardDiceData, standard_dam_mod: standardDamageMod, first_hit_die_list: firstHitDiceData, 
+        const player_character = {name, userId, description, crit_range: critRange, attack_mod: attackMod, num_of_attacks: numOfAttacks, has_adv: hasAdv, standard_die_list: standardDiceData, standard_dam_mod: standardDamageMod, first_hit_die_list: firstHitDiceData, 
             first_hit_dam_mod: firstHitDamageMod, crit_die_list: criticalDiceData, crit_dam_mod: criticalDamageMod, gwm_check: gwmCheck, lucky_check: luckyCheck, elven_acc_check: eAccCheck, crusher_check: crusherCheck,
                 piercer_check: piercerCheck, piercer_die: piercerDie, piercer_reroll: piercerRerollWhen, stalkers_flurry_check: stalkersFlurryCheck, graze_check: grazeCheck, graze_mod: grazeMod, vex_check: vexCheck, has_bonus_action: hasBonusAction, 
                 attack_mod_bonus: attackModifierBonus, num_of_attacks_bonus: numOfAttacksBonus, has_adv_bonus: hasAdvBonus, standard_die_list_bonus: standardDiceBonusData, standard_dam_mod_bonus: standardDamageModBonus, 
@@ -403,13 +430,22 @@ function Calc() {
     }
 
     useEffect(() => {
+        fetch("https://dlf-5e-dpr-calculator-backend.onrender.com/player_character/get/"+userId)
+        .then(res=>res.json())
+        .then(result => setCharactersList(result))
+        return () => {
+            console.log("Character Data Returned")
+        }
+    },[listLength, userId]);
+
+    /* useEffect(() => {
         fetch("https://dlf-5e-dpr-calculator-backend.onrender.com/player_character/getAll")
         .then(res=>res.json())
         .then(result => setCharactersList(result))
         return () => {
             console.log("Character Data Returned")
         }
-    },[listLength]);
+    },[listLength]); */
 
     const deleteCharacter = (id) => {
         fetch("https://dlf-5e-dpr-calculator-backend.onrender.com/player_character/remove/"+id,{
@@ -767,11 +803,22 @@ function Calc() {
 }
 
 function App() {
-    return (
-      <div className="App">
-        <Calc />
-      </div>
-    );
+    const [loggedIn, setLoggedIn] = useState(false)
+    const [currentUser, setCurrentUser] = useState('')
+    if (loggedIn === false) {
+        return (
+            <div className="App">
+                <LogIn theLogIn = {loggedIn} logInClick={(theLogIn, theUserId) => {setLoggedIn(!theLogIn); setCurrentUser(theUserId)}}/>
+            </div>
+            );
+    }
+    else {
+        return (
+        <div className="App">
+            <Calc userId = {currentUser}/>
+        </div>
+        );
+    }
   }
 
 export default App;
